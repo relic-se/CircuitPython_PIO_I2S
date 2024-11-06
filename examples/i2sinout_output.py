6fd1c56bade57e7f15ada40e4ebded0197667a93
dcooperdalrymple <dcooperdalrymple@gmail.com>
2024-11-06 12:54:12 -0600

    Improve output example.

--- examples/i2sinout_output.py
+++ examples/i2sinout_output.py
@@ -1,3 +1,4 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
 # SPDX-FileCopyrightText: Copyright (c) 2024 Cooper Dalrymple
 #
 # SPDX-License-Identifier: Unlicense
@@ -9,27 +10,29 @@ import board
 
 import i2sinout
 
+CHANNEL_COUNT = 2
+SAMPLE_RATE = 22050
+LENGTH = SAMPLE_RATE // 440
+
 codec = i2sinout.I2SInOut(
     bit_clock=board.GP0,  # word select is GP1
     data_out=board.GP3,
-    channel_count=2,
-    sample_rate=22050,
+    channel_count=CHANNEL_COUNT,
+    sample_rate=SAMPLE_RATE,
     bits_per_sample=16,
     samples_signed=True,
-    buffer_size=1024,
+    buffer_size=LENGTH * CHANNEL_COUNT,
 )
 
 # Generate one period of sine wave
-length = codec.sample_rate // 440
-sine_wave = array.array(codec.buffer_format, [0] * codec.buffer_size)
-for i in range(codec.buffer_size // codec.channel_count):
+sine_wave = array.array(codec.buffer_format, [0] * LENGTH * CHANNEL_COUNT)
+for i in range(LENGTH):
     value = int(
-        math.sin(math.pi * 2 * i / length) * ((2 ** (codec.bits_per_sample - 1)) - 1)
+        math.sin(math.pi * 2 * i / LENGTH) * ((2 ** (codec.bits_per_sample - 1)) - 1)
         + (2 ** (codec.bits_per_sample - 1) if not codec.samples_signed else 0)
     )
-    for j in range(codec.channel_count):
-        sine_wave[i * codec.channel_count + j] = value
+    for j in range(CHANNEL_COUNT):
+        sine_wave[i * CHANNEL_COUNT + j] = value
 
 # Write sine wave continuously
 codec.write(sine_wave, loop=True)
-# NOTE: There will be distortion in the sine_wave as it loops across the bounds of the buffer
